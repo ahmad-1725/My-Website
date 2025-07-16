d2ae4c3fa71a5f83f8a4b13a89a05a55e8fac05b
Ahmad <ahmadd.ilyas25@gmail.com>
2025-07-16 15:49:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var typed = new Typed(".text", {
+    strings: ['Frontend Developer', 'IT Student', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+});
